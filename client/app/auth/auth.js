09e0ad1d9ea470e75f3d2b8223d1cd52cfbe0ca2
Sara <sgee03@gmail.com>
2016-04-04 20:10:33 -0700

    controller

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,10 +1,25 @@
 angular.module('note.auth', [])
 
-.controller('AuthController', function ($scope, $location) {
+.controller('AuthController', function ($scope, $location, Auth) {
 
   $scope.signup = function() {
-    console.log($scope.first)
-    //$location.path('/notes')
+    Auth.currentUser($scope.first);
+    $location.path('/notes')
   }
+
   
+})
+
+ .factory('Auth', function () {
+  var firstname = [];
+
+  var currentUser = function (user) {
+    firstname.push(user);
+    console.log(firstname)
+  }
+
+  return {
+    firstname: firstname,
+    currentUser: currentUser
+  }  
 })
