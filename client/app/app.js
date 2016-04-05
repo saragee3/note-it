06b7b474defe42252f183ecc0aa0e23b4fecb0ef
Sara <sgee03@gmail.com>
2016-04-05 15:20:11 -0700

    add httpprovider

--- client/app/app.js
+++ client/app/app.js
@@ -1,9 +1,11 @@
 angular.module('note', [
   'note.auth',
   'note.notes',
+  'note.facts',
+  'note.services',
   'ngRoute'
   ])
-.config(function ($routeProvider) {
+.config(function ($routeProvider, $httpProvider) {
   $routeProvider
   .when('/', {
     templateUrl: 'app/auth/signup.html',
@@ -13,4 +15,12 @@ angular.module('note', [
     templateUrl: 'app/notes/notes.html',
     controller: 'NotesController'
   })
+  .when('/funfacts', {
+  templateUrl: 'app/facts/facts.html',
+  controller: 'FactsController'
+
+  })
+
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
 })
