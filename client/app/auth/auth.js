93243e83b23901efec29396331f34aba012ee739
Sara <sgee03@gmail.com>
2016-04-13 18:11:43 -0700

    Send username & PW to services

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,10 +1,15 @@
-angular.module('note.auth', [])
+angular.module('note.auth', ['note.services'])
 
-.controller('AuthController', function ($scope, $location, Auth) {
+.controller('AuthController', function ($scope, $location, Auth, Http) {
+
 
   $scope.signup = function() {
+    $scope.user = {};
     Auth.currentUser($scope.first);
-    $location.path('/notes')
+    $scope.user = {username: $scope.newuser, 
+                    password: $scope.pass};
+    Http.signUpToDB($scope.user);
+    $location.path('/notes');
   }
 })
 
