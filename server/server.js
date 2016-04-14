98673b486425af62cc9b08e306d1e29c32087463
Sara <sgee03@gmail.com>
2016-04-13 18:18:24 -0700

    Sign up POST req

--- server/server.js
+++ server/server.js
@@ -1,13 +1,34 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var parser = require('body-parser');
+var UserController = require('./users/userController')
+
+
+//mongoose connection
+//process.env.MONGOLAB_URI
+var mongoUri = 'mongodb://localhost/noteit';
+mongoose.connect(mongoUri);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console,'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected');
+});
 
 var app = express();
 
+//middleware
+app.use(morgan('dev'));
+app.use(parser.json());
 app.use(express.static(__dirname + '/../client'));
+
+app.post('/users', UserController.signup);
 
 var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
-  console.log("Listening to: " + port)
-})
+  console.log("Listening to: " + port);
+});
 
 module.exports = app;
